7904bac623b325a7ebe751e5ec08865ccf146091
ZachData24 <zechariasa@gmail.com>
2024-09-10 13:11:38 -0700

    Update server.js

--- cx/telegram/server.js
+++ cx/telegram/server.js
@@ -7,7 +7,7 @@
 // const locationId = 'global';
 // const agentId = 'my-agent';
 // const languageCode = 'en'
-// const TELEGRAM_TOKEN='1234567898:ABCdfghTtaD8dfghdfgh45sdf65467M';
+// const TELEGRAM_TOKEN='6981450930:AAEoW8uH6mtEwWAWbHqdEr0u3FR5KFDfi28';
 // const SERVER_URL='https://example.com';
 
 const structProtoToJson =
